45aef623362b302c7e51d23846548888c74c252d
tn-anhthu <anhthutran.tnat@gmail.com>
2026-07-12 01:37:10 +0700

    Wire up dark mode toggle: anti-flash inline script + persisted theme.js

---
+++ assets/js/theme.js
@@ -0,0 +1,26 @@
+(function () {
+  "use strict";
+
+  var toggle = document.getElementById('theme-toggle');
+  var icon = toggle.querySelector('i');
+
+  function applyIcon(theme) {
+    if (theme === 'dark') {
+      icon.classList.remove('bx-moon');
+      icon.classList.add('bx-sun');
+    } else {
+      icon.classList.remove('bx-sun');
+      icon.classList.add('bx-moon');
+    }
+  }
+
+  applyIcon(document.documentElement.getAttribute('data-theme'));
+
+  toggle.addEventListener('click', function () {
+    var current = document.documentElement.getAttribute('data-theme');
+    var next = current === 'dark' ? 'light' : 'dark';
+    document.documentElement.setAttribute('data-theme', next);
+    localStorage.setItem('theme', next);
+    applyIcon(next);
+  });
+})();
